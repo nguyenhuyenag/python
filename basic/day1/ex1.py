74a57179ae6b897975aac815c7b2b9093b002894
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-14 09:35:52 +0700

    #

--- basic/day1/ex1.py
+++ basic/day1/ex1.py
@@ -1,14 +1,18 @@
+import time
+
+
 def f(n):
     if n < 2:
         return n
     return f(n-1)+f(n-2)
+
+
 print(f(5))
 
-import time
 start = time.process_time()
 # code here
 print(f(10))
-#end code
+# end code
 end = time.process_time()
 print(end - start)
 
