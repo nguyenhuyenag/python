888498380d4f0d5e505aba3f6e67912c3d4d4f2b
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-18 14:35:16 +0700

    #

---
+++ python-core/com/number.py
@@ -0,0 +1,14 @@
+# Số phức được viết với hậu tố "j" hoặc "J", (real+imagJ)
+z = 1j * 1J
+
+# Hoặc
+z = complex(1, 2)
+print(z)
+
+# Để lấy các phần từ một số phức z, dùng z.real và z.imag
+print(z.real, z.imag)
+
+# Lấy đội dài
+print(abs(z))
+
+# Trong Python shell, dấu `_` sẽ lưu giữ kết quả của phép tính trước đó
---
+++ python-core/com/string.py
@@ -0,0 +1,6 @@
+word = 'Help' + 'A'
+print(5 * word)
+
+# Hai chuỗi đặt kế tiếp nhau sẽ tự động nối lại
+print('a' 'b')
+print("a""b")
