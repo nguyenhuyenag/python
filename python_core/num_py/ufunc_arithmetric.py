04f9fd8c8094d3b98783bc704674651af577da18
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-02-10 14:40:57 +0700

    #

--- python_core/num_py/ufunc_arithmetric.py
+++ python_core/num_py/ufunc_arithmetric.py
@@ -6,13 +6,22 @@ a2 = [1, 2, 3, 4]
 
 # a1 + a2
 print(np.add(a1, a2))
+
 # a1 - a2
 print(np.subtract(a1, a2))
+
 # a1 * a2
 print(np.multiply(a1, a2))
+
 # a1 / a2
 print(np.divide(a1, a2))
+
 # a1 ^ a2
 print(np.power(a1, a2))
+
 # a1 mod a2 = a1 // a2
 print(np.mod(a1, a2))
+
+# = hàm abs()
+arr = np.array([-1, -2, 1, 2, 3, -4])
+print(np.absolute(arr))
---
+++ python_core/num_py/ufunc_rouding.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = np.array([1.253, 7.08])
+
+# Làm tròn xuống
+print(np.floor(arr))
+
+# Làm tròn lên
+print(np.ceil(arr))
+
+print(np.around(arr))
+
+print(np.trunc([-3.1666, 3.6667]))
+
+print(np.fix([-3.1666, 3.6667]))
+
