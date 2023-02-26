7774e428c39c49e0b443af4b5bcf22736fb9033e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-02-26 20:18:51 +0700

    #

--- python_core/file/file_write.py
+++
@@ -1,8 +0,0 @@
-# To write to an existing file, you must add a parameter to the open() function:
-#   'x' - Create, will create a file, returns an error if the file exist
-#   'a' - Append, will append to the end of the file
-#   'w' - Write, will overwrite any existing content
-
-f = open("data-file.txt", "w")
-f.write("Now the file has more content!\n")
-f.close()
