b460f7b566578eb845ebc79ff161d5f694476fc9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-03-03 14:51:52 +0700

    #

--- python_core/amain.py
+++ python_core/amain.py
@@ -1,7 +1,5 @@
-# from dbsettings import connection_properties
-#
-# try:
-#     self.conn = pymysql.connect(**connection_properties)
-#     self.curr = self.conn.cursor()
-# except pymysql.err.OperationalError :
-#     sys.exit("Invalid Input: Wrong username/database or password found, please try again")
+from mysql.dbsettings import connection_properties
+
+x  = connection_properties
+
+print(*x)
