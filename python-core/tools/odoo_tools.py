297618696ff20c3934c76e19e057288ef2be4193
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-19 17:08:19 +0700

    #

--- python-core/tools/odoo_tools.py
+++ python-core/tools/odoo_tools.py
@@ -5,6 +5,5 @@ fields = file_utils.read_file_into_list(r'table_data.txt')
 # fields = ['name', 'id', 'message_main_attachment_id', 'write_date', 'x_property_id']
 
 # fields.sort()
-# print(fields)
-
-create_json_pattern_from_array.create_json(fields, "template")
+print(fields)
+# create_json_pattern_from_array.create_json(fields, "template")
