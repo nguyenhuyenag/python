47c8a4e7357966875c7a3e2b784179a0621c53d2
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-07-11 11:14:48 +0700

    #

--- python-core/main.py
+++ python-core/main.py
@@ -1,1 +1,7 @@
-print(int('06'))
+from datetime import datetime
+
+date_string = "03/19/2021"
+date_object = datetime.strptime(date_string, "%m/%d/%Y")
+formatted_date = date_object.strftime("%Y-%m-%d")
+
+print(formatted_date)
