8008e6c8e0690c245c81a6b8551da5755b80a387
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-09-25 21:21:07 +0700

    #

--- python-core/path_file/excel/import_to_mysql.py
+++ python-core/path_file/excel/import_to_mysql.py
@@ -1,51 +1,50 @@
 import openpyxl
-import xlrd
 import mysql.connector
+from datetime import datetime
+
+
+def to_mysql_datetime(str_date, pattern='%d/%m/%Y'):
+    str_date = datetime.strptime(str_date, pattern)
+    return str_date.strftime('%Y-%m-%d')
+
 
 db_config = {
-    "host": "localhost:3306",
+    "host": "localhost",
+    "port": "3306",
     "user": "root",
     "password": "root",
     "database": "test_db",
 }
 
-# connection = mysql.connector.connect(**db_config)
-# Get the cursor, which is used to traverse the database, line by line
-# cursor = connection.cursor()
+connection = mysql.connector.connect(**db_config)
+
+# if connection.is_connected():
+#     print("Kết nối đến MySQL thành công")
+# else:
+#     print("Kết nối đến MySQL thất bại")
+
+cursor = connection.cursor()
+# print(cursor)
+
 
 workbook = openpyxl.load_workbook(r'DATA_mau-2.xlsx')
 sheet_name = workbook['Template Phatsinh']
 all_rows = sheet_name.iter_rows(min_row=2, values_only=True)
 
+sql_query = "INSERT INTO tra_soat (hoten, so_bhxh, ngaysinh, tu_thangnam) VALUES (%s, %s, %s, %s)"
+
 for (i, row) in enumerate(all_rows, start=2):
-    if row:
-        print(i, row)
+    # print(i, row)
+    hoten       = row[1]
+    so_bhxh     = row[2]
+    ngaysinh    = to_mysql_datetime(row[3])
+    tu_thangnam = to_mysql_datetime(row[4], '%m/%Y')
 
-# Create the INSERT INTO sql query
-query = "INSERT INTO orders (ID, hovaten, sobhxh, ngaysinh, tuthang, denthang, tylebosung, trathe, ghichu, ngaytao_ps, nguoitao_ps, nguoi_import, ngay_impore,
-) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (hoten, so_bhxh, ngaysinh, tu_thangnam)
+    # print(values)
 
-# Create a For loop to iterate through each row in the XLS file, starting at row 2 to skip the headers
-# for r in range(1, sheet.nrows):
-#     print(sheet.cell(r, ).value)
-#     print(sheet.cell(r, 1).value)
-    # product = sheet.cell(r, ).value
-    # customer = sheet.cell(r, 1).value
-    # rep = sheet.cell(r, 2).value
-    # date = sheet.cell(r, 3).value
-    # actual = sheet.cell(r, 4).value
-    # expected = sheet.cell(r, 5).value
-    # open = sheet.cell(r, 6).value
-    # closed = sheet.cell(r, 7).value
-    # city = sheet.cell(r, 8).value
-    # state = sheet.cell(r, 9).value
-    # zip = sheet.cell(r, 10).value
-    # pop = sheet.cell(r, 11).value
-    # region = sheet.cell(r, 12).value
+    cursor.execute(sql_query, values)
 
-    # values = (product, customer, rep, date, actual, expected, open, closed, city, state, zip, pop, region)
-    # cursor.execute(query, values)
-
-# cursor.close()
-# connection.commit()
-# connection.close()
+cursor.close()
+connection.commit()
+connection.close()
