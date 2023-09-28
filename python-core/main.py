0fc0d8e93f9a41d4cac6923db2347a904d949b52
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-09-28 17:06:40 +0700

    #

--- python-core/main.py
+++ python-core/main.py
@@ -1,5 +1,1 @@
-A = ['city', 'coordinates_calc', 'country', 'create_date', 'create_uid', 'display_name', 'id', 'name', 'store_email',
- 'store_lat', 'store_long', 'store_mob', 'store_phone', 'store_state', 'street', 'write_date', 'write_uid',
- 'x_company_id', 'zipcode']
-
-print(len(A))
+print(2505.66 + 2480.14)
