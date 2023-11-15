30b476bc35591021273b8e6c4a98eaf2686d0cea
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-11-15 08:04:54 +0700

    #

--- python-core/tools/odoo_tools.py
+++ python-core/tools/odoo_tools.py
@@ -1,5 +1,5 @@
 from path_file import file_utils
-from collection_data.dictionary_json import create_json_pattern_from_array
+from collection_data.dictionary_json import json_create_from_pattern_array
 
 fields = file_utils.read_file_into_list(r'table_data.txt')
 # fields = ['name', 'id', 'message_main_attachment_id', 'write_date', 'x_property_id']
