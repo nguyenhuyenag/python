01f13cdec9d63afb18d46c2f20cf466f6636dad1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-22 10:38:52 +0700

    #

---
+++ python-core/loop/loop_matrix.py
@@ -0,0 +1,8 @@
+arr = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+# for (a1, a2, a3,...) in arr: Tương ứng với số lượng phần tử của một dòng
+for (a, b, c) in arr:
+    print(str(a) + " " + str(b) + " " + str(c))
