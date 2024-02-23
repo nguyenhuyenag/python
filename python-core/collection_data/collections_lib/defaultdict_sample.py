7b379e050ffacde6a395861790e5b5442998af83
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-23 16:33:49 +0700

    #

--- python-core/collection_data/collections_lib/defaultdict_sample.py
+++ python-core/collection_data/collections_lib/defaultdict_sample.py
@@ -1,4 +1,20 @@
+import collections
 from collections import defaultdict
+
+"""
+- Default dictionary: Khi truy cập một khóa chưa tồn tại, nó sẽ tự động thêm khóa đó vào từ điển 
+và thiết lập giá trị mặc định là 0 (hoặc giá trị được chỉ định khi bạn tạo defaultdict):
+
+    my_dict = defaultdict(int)
+    
+    my_dict['a'] += 1
+    my_dict['b'] += 2
+    
+    # Không cần kiểm tra khóa tồn tại trước khi thay đổi giá trị
+    my_dict['c']  # Giá trị mặc định là 0, không cần phải thêm 'c' vào trước đó
+    
+    print(my_dict)  # Output: defaultdict(<class 'int'>, {'a': 1, 'b': 2, 'c': 0})
+"""
 
 
 def normal():
@@ -15,7 +31,7 @@ def normal():
 
 def using_defaultdict():
     arr = 'abdacvcdcscbcbgnmnnb'
-    counts = defaultdict(lambda: 0)
+    counts = collections.defaultdict(lambda: 0)
     for key in arr:
         counts[key] += 1
 
