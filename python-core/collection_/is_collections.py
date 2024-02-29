bc0ff346d155a827dea723f69838f37df6be8150
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-29 22:26:02 +0700

    #

--- python-core/collection_/is_collections.py
+++ python-core/collection_/is_collections.py
@@ -1,9 +1,11 @@
 from collections.abc import Iterable
 
+
 # is collections
 def is_collections():
     print("[] -> ", isinstance([], Iterable))
     print("{} -> ", isinstance({}, Iterable))
     print("None -> ", isinstance(None, Iterable))
+
 
 is_collections()
