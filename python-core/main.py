a56981e1ddbba1060deed027bcfe8acb08985f74
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-05 21:58:24 +0700

    #

--- python-core/main.py
+++ python-core/main.py
@@ -1,1 +1,19 @@
-print("Zfill:", "new_one".zfill(10))
+s = "-abc+3a+2ac"
+
+result = []
+current_term = ""
+for char in s:
+    if char in "+-" and current_term:
+        result.append(current_term)
+        current_term = char
+    else:
+        current_term += char
+
+# Thêm phần tử cuối cùng vào kết quả
+if current_term:
+    result.append(current_term)
+
+# Loại bỏ dấu "+" ở đầu các thành phần không mang ý nghĩa
+result_array = [term[1:] if term.startswith('+') else term for term in result]
+
+print(result_array)
