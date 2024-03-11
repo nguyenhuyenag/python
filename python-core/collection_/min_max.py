d1111b4f19299534c86f6622bb343e0fd05b7aa3
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-11 20:36:10 +0700

    #

---
+++ python-core/collection_/min_max.py
@@ -0,0 +1,5 @@
+"""
+    Tìm arr không có phần tử nào thì sẽ trả về giá trị default
+"""
+arr = [1, 2, 3]
+min(arr, default=-1)
--- python-core/main.py
+++
@@ -1,9 +0,0 @@
-import math
-import sys
-max_size = sys.maxsize
-min_size = -sys.maxsize - 1
-
-print(min_size)
-print(max_size)
-
-print(math.inf)
