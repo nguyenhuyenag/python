66a0c497496f2b500f0a2e9c33bc2af1cc931ef8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-18 21:16:23 +0700

    #

--- python-core/main.py
+++ python-core/main.py
@@ -2,13 +2,13 @@ def tower(n, a, b, c):
     if n == 1:
         print(f"{a} -----> {c}")
         return
-
-    # Dùng c làm trung gian, di chuyển n - 1 đĩa từ a -> b
-    tower(n - 1, a, c, b)
-    # Cho đến khi chỉ còn 1 đĩa ở a, thì dời nó sang c
-    tower(1, a, b, c)
-    # Dùng a làm trung gian, dời n - 1 đĩa từ b sang c
-    tower(n - 1, b, a, c)
+    else:
+        # Dùng c làm trung gian, di chuyển n - 1 đĩa từ a -> b
+        tower(n - 1, a, c, b)
+        # Cho đến khi chỉ còn 1 đĩa ở a, thì dời nó sang c
+        tower(1, a, b, c)
+        # Dùng a làm trung gian, dời n - 1 đĩa từ b sang c
+        tower(n - 1, b, a, c)
 
 
 if __name__ == '__main__':
