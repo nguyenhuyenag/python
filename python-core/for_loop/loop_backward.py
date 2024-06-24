f3f9b681185f932db5a8d01025dfab488aa1ed34
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-24 17:16:41 +0700

    #

--- python-core/for_loop/loop_backward.py
+++ python-core/for_loop/loop_backward.py
@@ -2,22 +2,23 @@
     Loop backwards
 """
 
-n = 5
-
 def _range_three_params():
-    # [5, 4, 3, 2, 1]
-    print(list(range(n, -1, -1)))
+    arr = [8, 0, 10, 5, 77]
+    for i in range(len(arr) - 1, -1, -1):
+        print(f"index={i}, value={arr[i]}")
 
 
 def _reverse():
     # [5, 4, 3, 2, 1, 0]
+    n = 5
     print(list(reversed(range(n + 1))))
 
 
 def _slice_syntax():
+    n = 5
     print(list(range(n + 1)[::-1]))
 
 
 _range_three_params()
-_reverse()
+# _reverse()
 # _slice_syntax()
