9f90422d994c68194a370380d386fb984d1103d8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-06-11 21:59:55 +0700

    #

--- python-tools/chi_tieu/chi_tieu_execute.py
+++ python-tools/chi_tieu/chi_tieu_execute.py
@@ -12,7 +12,7 @@ def open_excel_with_office(file_path):
 # Load the Excel workbook
 file_path = r'D:\WORK\Cloud\FirstDrive\Zother\chi_tieu\chi-tieu.xlsx'
 workbook = openpyxl.load_workbook(file_path)
-sheet_name = workbook['Th3']  # Replace 'Sheet1' with your sheet name
+sheet_name = workbook['Th5']  # Replace 'Sheet1' with your sheet name
 
 b_index = 1  # 0 -> A, 1 -> B,...
 c_index = 2
