fe7b429c33a4c14f4ff91e54bf0f3397bd28757c
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2026-03-30 10:06:34 +0700

    Auto commit

---
+++ python-core/basic/syntax.py
@@ -0,0 +1,10 @@
+"""
+    Python Syntax:
+
+        Logical Operators
+            or, and, not
+
+        Boolean Operators
+            ==, !=, >, <, >=, <=, is, is not
+            True, False
+"""
