ea2d519b85304e0326597aeaedb9fe918dbc8b3a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2026-04-15 16:55:00 +0700

    Auto commit

--- python-core/main.py
+++ python-core/main.py
@@ -1,50 +1,9 @@
-import base64
-
 import requests
-import json
-from requests.auth import HTTPBasicAuth
 
-headers = {
-    'Content-Type': 'application/json'
-}
+url = ('http://192.168.11.69:8080/ts24id_ws_v1/IDTS24.rest/loginAuthentication?userAgent=ts24pro&passAgent=d1663225d1689433cb6943621635189a'
+       '&userAccount=huyennv@gmail.com'
+       '&passAccount=e10adc3949ba59abbe56e057f20f883e_1')
 
-# Function to convert file to base64
-def file_to_base64(file_path):
-    with open(file_path, "rb") as file:
-        # Read file content and encode it to Base64
-        encoded_string = base64.b64encode(file.read()).decode('utf-8')
-    return encoded_string
-
-file_path = "C:/Users/huyennv/Desktop/ts24pro_mobile/CauTruc_ContractSigning_Ky_base64.xml"
-
-# byte_array = read_file_to_byte_array(file_path)
-# file_to_base64
-# encoded_byte_array = base64.b64encode(byte_array).decode('utf-8')
-
-payload = {
-    "oDTrinhKy": {
-				"maSoThue": "12344",
-				"coQuanThue": "",
-				"ngayTrinhKy": "2024-10-10",
-				"tenfile":"daky.cs",
-				"lanGui":0,
-				"taiKhoanTrinhKy":"la.le@gmail.com",
-				"taiKhoanKyChinh":"la.le@gmail.com",
-				"taiKHoanKyDuyet":"",
-				"tinhTrang":0,
-				"dungLuong":0,
-				"noiDung":"",
-				"loai":"",
-				"tinhTrangXuLy":""
-			},
-    "fileContentBase64": file_to_base64(file_path),
-    "loaiTrinhKy": 1
-}
-
-auth = HTTPBasicAuth('ts24one', 'fd40f21f61fb4806587216692c30a1e1')
-url = "http://localhost:8080/ts24pro_mobile/HD_MOBILE/addDownloadTrinhKy_HD?sKey=AxNC0xMC0wMyAxNzowODozN35fmZ1dkYyNktEcE15bnFBZUhUTHhZaE1VbzNMTT"
-
-response = requests.post(url, auth=auth, headers=headers, data=json.dumps(payload))
-
-print("Status:", response.status_code)
-print(json.dumps(response.json(), indent=4, ensure_ascii=False))
+for i in range(4):
+    response = requests.post(url)
+    print(response.text)
